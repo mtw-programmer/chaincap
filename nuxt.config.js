fae0ab5f05579c334494735ad3ee22facecefc88
mtw-programmer <visnia58@gmail.com>
2023-12-28 14:52:09 +0100

    chore: replace axios with @apollo/client and graphql

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,16 +35,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
+  modules: [],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
