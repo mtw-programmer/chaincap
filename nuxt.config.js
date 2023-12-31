894b7e79df27fa39a87fc6c1bb4145c111d63031
mtw-programmer <visnia58@gmail.com>
2023-12-31 14:17:43 +0100

    chore: update website title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - chaincap-vue',
-    title: 'chaincap-vue',
+    title: 'ChainCAP Ventures',
     htmlAttrs: {
       lang: 'en',
     },
