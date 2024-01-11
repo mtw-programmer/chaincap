8197ff58fef3bc3c507abe3e0157e2388e7c763c
mtw-programmer <visnia58@gmail.com>
2024-01-11 21:06:46 +0100

    chore: update nuxt config
    properly define head and get rid of vuetify configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,30 @@
-import colors from 'vuetify/es5/util/colors'
+import { defineNuxtConfig } from 'nuxt/config';
 
-export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'ChainCAP Ventures',
-    htmlAttrs: {
-      lang: 'en',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
-    script: [{
-      hid: 'font-awesome',
-      src: 'https://kit.fontawesome.com/0c93a334b2.js',
-      crossorigin: 'anonymous'
-    }],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-
+export default defineNuxtConfig({
   store: '~/store/index.ts',
+
+  app: {
+    head: {
+      title: 'ChainCAP Ventures',
+        htmlAttrs: {
+          lang: 'en',
+        },
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { hid: 'description', name: 'description', content: '' },
+          { name: 'format-detection', content: 'telephone=no' },
+        ],
+        script: [
+          {
+            hid: 'font-awesome',
+            src: 'https://kit.fontawesome.com/0c93a334b2.js',
+            crossorigin: 'anonymous',
+          },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
 
   router: {
     middleware: ['authenticated'],
@@ -40,32 +43,8 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
-
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ['~/assets/css/variables/vuetify.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+});
