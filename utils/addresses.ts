7da600477336b4cb99ee8912acea2fad92147ec1
mtw-programmer <visnia58@gmail.com>
2024-01-21 22:31:36 +0100

    feat: create util for addresses

---
+++ utils/addresses.ts
@@ -0,0 +1,44 @@
+import { provideApolloClient, useQuery } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
+
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { createHttpLink } from '@apollo/client/link/http';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  credentials: 'include',
+});
+
+provideApolloClient(apolloClient);
+
+export const getAddresses = async ():Promise<any> => {
+  try {
+    const authenticationQuery = gql`
+      query addresses {
+        addresses {
+          status,
+          msg,
+          address {
+            expert,
+            regular
+          }
+        }
+      }
+    `;
+
+    const response = useQuery(authenticationQuery, {
+      notifyOnNetworkStatusChange: true,
+      retry: false,
+      suspense: false,
+    });
+
+    return response;
+  } catch (ex) {
+    return { error: `Couldn't authenticate with your wallet.` };
+  }
+};
